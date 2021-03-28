cd848a1cca4bbc88a4676b086da03ad5581e6b07
manoranjaan <manoranjaan13@gmail.com>
2021-03-28 17:35:37 +0530

    aaa

--- Testproject/Testproject/settings.py
+++ Testproject/Testproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qxsp@y+v3y-if5(nm8xom5imxr%9rs69&_v5ngo4e8yj5d&ado'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'Testproject',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Testproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/sandy/Desktop/test-git/Testproject/Testproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]    
+
--- Testproject/Testproject/urls.py
+++ Testproject/Testproject/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
+
 urlpatterns = [
-    path('home',views.home, name='home'),
+    path('',views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
